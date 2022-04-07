e4a971d6e45592f57f6abacb4063b07bd2e29a9b
p15baka1 <p15baka1@ionio.gr>
2022-04-07 15:56:01 +0300

    GameOverMenu

--- Assets/Scripts/Player Scripts/PlayerStuff.cs
+++ Assets/Scripts/Player Scripts/PlayerStuff.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public enum PlayerState
 {
@@ -179,6 +180,7 @@ public class PlayerStuff : MonoBehaviour
         else
         {
             this.gameObject.SetActive(false);
+            SceneManager.LoadScene("GameOverMenu");
         }
     }
 
