9394e3d2fe84f67d12ecfc738f3abd7d04262a1c
p15baka1 <p15baka1@ionio.gr>
2022-05-05 17:36:18 +0300

    Level 1 setup

--- Assets/Scripts/Enemy Scripts/Log.cs
+++ Assets/Scripts/Enemy Scripts/Log.cs
@@ -61,4 +61,11 @@ public class Log : Enemy
             currentState = newState;
         }
     }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, chaseRadius);
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+    }
 }
