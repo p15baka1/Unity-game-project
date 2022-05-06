5c85ceed67be7119ab6b8d2c7d999c6be5d77432
p15baka1 <p15baka1@ionio.gr>
2022-05-06 18:23:26 +0300

    fixes

---
+++ Assets/Scripts/Game Scripts/TimelineTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TimelineTrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeline = GetComponent<PlayableDirector>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            timeline.Play();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            timeline.Stop();
+            Destroy(this.gameObject);
+        }
+    }
+}
