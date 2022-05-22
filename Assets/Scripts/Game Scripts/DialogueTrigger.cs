57aa83278f5dc827970853b93b64cf5c556fbb88
p15baka1 <p15baka1@ionio.gr>
2022-05-22 13:21:34 +0300

    tutorial fixes

--- Assets/Scripts/Game Scripts/DialogueTrigger.cs
+++ Assets/Scripts/Game Scripts/DialogueTrigger.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public GameObject pausePanel;
+
+     void Update()
+    {
+        pausePanel.SetActive(true);
+        Time.timeScale = 0f;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            pausePanel.SetActive(false);
+            Time.timeScale = 1f;
+            TriggerDialogue();
+            Destroy(gameObject);
+        }
+    }
 
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
     }
 
-    public void DestroyButton()
-    {
-        Destroy(gameObject);
-    }
+
 }
