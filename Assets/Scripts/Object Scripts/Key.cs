6eddd208cc7608c61a7cafaa1df9f9478a7b9a79
p15baka1 <p15baka1@ionio.gr>
2022-06-20 14:47:27 +0300

    key prefab

---
+++ Assets/Scripts/Object Scripts/Key.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : PowerUp
+{
+    public Inventory playerInventory;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            playerInventory.numberOfKeys += 1;
+            powerupSignal.Raise();
+            Destroy(this.gameObject);
+            FindObjectOfType<AudioManager>().Play("Coin");
+        }
+    }
+}
