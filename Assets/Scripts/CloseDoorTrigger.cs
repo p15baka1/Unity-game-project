21c5a62cf301984b24c1dd8f96b8b0698cc209cd
p15baka1 <p15baka1@ionio.gr>
2022-06-20 13:43:06 +0300

    trap door

---
+++ Assets/Scripts/CloseDoorTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseDoorTrigger : MonoBehaviour
+{
+    public GameObject door;
+    public BoxCollider2D trigger;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            door.SetActive(true);
+            FindObjectOfType<AudioManager>().Play("CloseDoor");
+            trigger.enabled = false;
+        }
+    }
+}
