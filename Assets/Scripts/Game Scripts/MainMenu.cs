d6ec3c25ea8226fa07bdf387a0f6c7e600528216
p15baka1 <p15baka1@ionio.gr>
2022-06-21 14:53:41 +0300

    reset inventory in new game

--- Assets/Scripts/Game Scripts/MainMenu.cs
+++ Assets/Scripts/Game Scripts/MainMenu.cs
@@ -3,9 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public Inventory playerInventory;
+    public Item bow;
     public void NewGame()
     {
         SceneManager.LoadScene("SampleScene");
+        playerInventory.coins = 0;
+        playerInventory.numberOfKeys = 0;
+        playerInventory.items.Remove(bow);
     }
 
     public void Restart()
