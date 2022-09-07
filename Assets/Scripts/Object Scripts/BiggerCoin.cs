ff0cecc12c31940c974fbb81c2576c3b5760aa4e
p15baka1 <p15baka1@ionio.gr>
2022-09-07 17:33:16 +0300

    PurpleCoin

---
+++ Assets/Scripts/Object Scripts/BiggerCoin.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class BiggerCoin : PowerUp
+{
+    public Inventory playerInventory;
+    // Start is called before the first frame update
+    void Start()
+    {
+        powerupSignal.Raise();
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            playerInventory.coins += 10;
+            powerupSignal.Raise();
+            Destroy(this.gameObject);
+            FindObjectOfType<AudioManager>().Play("PurpleCoin");
+        }
+    }
+}
