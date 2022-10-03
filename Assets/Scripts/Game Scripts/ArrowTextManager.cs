8e3e03fd3d6aca33013ce7e6e5256a9a5df3aa49
p15baka1 <p15baka1@ionio.gr>
2022-10-03 15:59:58 +0300

    Arrow text manager

---
+++ Assets/Scripts/Game Scripts/ArrowTextManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowTextManager : MonoBehaviour
+{
+    public Inventory playerInventory;
+    public Text arrowDisplay;
+
+    private void Start()
+    {
+        UpdateArrowCount();
+    }
+
+    private void Update()
+    {
+        UpdateArrowCount();
+    }
+
+
+    public void UpdateArrowCount()
+    {
+        arrowDisplay.text = playerInventory.currentArrows.ToString("00");
+    }
+
+}
